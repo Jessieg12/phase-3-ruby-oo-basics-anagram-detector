2149faed6c64e49bc8f235d3e86e57107ea1472d
Jessie Guzman <jessieguzman777@gmail.com>
2023-12-11 15:08:23 -0600

    commit tree b42486930bec6e570bb2369a35fb8db27e604e74

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+
+require "pry"
+class Anagram
+  attr_reader :word
+  def initialize(word)
+    @word = word
+  end
+  def match(array)
+    #binding.pry
+    match_sorter = array.select do |w|
+      w.chars.sort == @word.chars.sort
+    end
+    return match_sorter
+  end
+end
+
+#listen = Anagram.new("listen")
+#listen.match(%w[enlists google inlets banana])
